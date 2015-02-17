a0eebf97e74e21fa2123987a427be57b5e8f3b88
Daniel Wirtz <daniel.wirtz@simtech.uni-stuttgart.de>
2015-02-17 13:07:50 +0100

    renamings

--- CMakeScripts/MPISetup.cmake
+++ CMakeScripts/MPISetup.cmake
@@ -2,7 +2,7 @@
 # This is supported yet only on Unix systems
 if (UNIX)
 
-    # Set the forward dependencies of MPI only if we build it ourselve
+    # Set the forward dependencies of MPI only if we build it ourselves
     SET(MPI_FWD_DEPS ${OPENCMISS_COMPONENTS_WITHMPI})
     # The choice is ... 
     if (MPI STREQUAL openmpi)
