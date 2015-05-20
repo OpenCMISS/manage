48db0d4c40c1443c44a072678ad3e909348b79ff
Daniel Wirtz <daniel.wirtz@simtech.uni-stuttgart.de>
2015-05-20 13:43:13 +0200

    started re-write of CMAke own builds, to be continued by Andreas

---
+++ CMakeScripts/CMakeBuild.cmake
@@ -0,0 +1,129 @@
+# This script is intended to be used within the OpenCMISS build environment, called from CMakeCheck.cmake.
+# Arguments passed to this script are:
+# OPENCMISS_ROOT
+# DCMAKE_MIN_VERSION_MAJ
+# DCMAKE_MIN_VERSION
+
+message(STATUS "Building CMake version ${CMAKE_MIN_VERSION} ..")
+# Use the cmake binary with which this script was invoked as default 
+SET(MY_CMAKE_COMMAND ${CMAKE_COMMAND})
+
+MACRO(BUILD_CMAKE VERSION_TO_BUILD VERSION_TO_BUILD_MAJ BUILD_WITH_OPENSSL)
+    # Download
+    if(NOT EXISTS ${CMAKE_SRC_DIR}/${CMAKE_TARBALL})
+        SET(CMAKE_SRC_TAR http://www.cmake.org/files/v${VERSION_TO_BUILD_MAJ}/${CMAKE_TARBALL})
+        message(STATUS "Downloading ${CMAKE_SRC_TAR}")
+        FILE(DOWNLOAD ${CMAKE_SRC_TAR} ${CMAKE_SRC_DIR}/${CMAKE_TARBALL})
+    endif()
+        
+    # Extract
+    message(STATUS "Extracting ${CMAKE_SRC_DIR}/${CMAKE_TARBALL} [${CMAKE_COMMAND} -E tar xzvf ${CMAKE_TARBALL}]")
+    execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${CMAKE_TARBALL} .
+        WORKING_DIRECTORY ${CMAKE_SRC_DIR})
+    file(REMOVE ${CMAKE_SRC_DIR}/${CMAKE_TARBALL})
+    
+    # Add top-level folder to src dir (default for cmake.org downloads)
+    SET(CMAKE_SRC_DIR "${CMAKE_SRC_DIR}/cmake-${VERSION_TO_BUILD}")
+    
+    # Build config
+    SET(CMAKE_DEFS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_DIR}
+        -DCMAKE_USE_OPENSSL=${BUILD_WITH_OPENSSL})
+    
+    # Create dir
+    SET(CMAKEBUILD_BINARY_DIR ${OPENCMISS_ROOT}/build/utilities/cmake-${VERSION_TO_BUILD})
+    file(MAKE_DIRECTORY ${CMAKEBUILD_BINARY_DIR})
+    
+    # Get build commands
+    include(OCMSetupBuildMacros)
+    GET_BUILD_COMMANDS(BUILD_COMMAND INSTALL_COMMAND ${CMAKEBUILD_BINARY_DIR} YES)
+    
+    # Run cmake
+    execute_process(COMMAND ${MY_CMAKE_COMMAND}
+        ${CMAKE_DEFS}
+        ${CMAKE_SRC_DIR}
+        WORKING_DIRECTORY ${CMAKEBUILD_BINARY_DIR}
+    )
+    
+    # Run build
+    #execute_process(COMMAND ${BUILD_COMMAND})
+    
+    # Run build/install
+    execute_process(COMMAND ${INSTALL_COMMAND})
+    
+    if (NOT EXISTS ${MY_CMAKE_EXECUTABLE})
+        message(FATAL_ERROR
+            "@@@@@@@@@ OpenCMISS utilities @@@@@@@@@\n"
+            "Building a newer CMake version failed. Please check & build manually.\n"
+            "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+        )
+    endif()
+ENDMACRO()
+     
+SET(CMAKE_INSTALL_DIR ${OPENCMISS_ROOT}/install/utilities/cmake)
+SET(MY_CMAKE_EXECUTABLE ${CMAKE_INSTALL_DIR}/bin/cmake${CMAKE_EXECUTABLE_SUFFIX})
+
+# Check if the binary is already there and hint the user to it
+if (NOT EXISTS ${MY_CMAKE_EXECUTABLE})
+
+    # Otherwise .. need compilation!
+    message(WARNING "Your CMake version is ${CMAKE_VERSION}, but at least version ${CMAKE_MIN_VERSION} is required for OpenCMISS. Building now..")
+    
+    SET(CMAKE_SRC_DIR ${OPENCMISS_ROOT}/src/utilities)
+    SET(CMAKE_INTERMEDIATE_VERSION_MAJ 2.8)
+    SET(CMAKE_INTERMEDIATE_VERSION ${CMAKE_INTERMEDIATE_VERSION_MAJ}.4)
+    
+    # compile intermediate version of cmake if present is too old
+    if (CMAKE_VERSION VERSION_LESS ${CMAKE_INTERMEDIATE_VERSION})
+        message(WARNING "Your CMake version is too old: ${CMAKE_VERSION}!\n"
+                        "A newer version is required to build version ${CMAKE_MIN_VERSION}. Building now..")
+        # set up the paths for an intermediate version of cmake
+        SET(CMAKE_TARBALL cmake-${CMAKE_INTERMEDIATE_VERSION}.tar.gz)
+        SET(CMAKE_INTERMEDIATE_VERSION_INSTALL_DIR ${OPENCMISS_ROOT}/install/utilities/cmake-${CMAKE_INTERMEDIATE_VERSION})
+        SET(CMAKE_INSTALL_DIR ${CMAKE_INTERMEDIATE_VERSION_INSTALL_DIR})
+        SET(MY_CMAKE_EXECUTABLE ${CMAKE_INSTALL_DIR}/bin/cmake${CMAKE_EXECUTABLE_SUFFIX})
+        # TODO: we could do a check again in case an intermediate version was built before?
+        BUILD_CMAKE(${CMAKE_INTERMEDIATE_VERSION} ${CMAKE_INTERMEDIATE_VERSION_MAJ} NO)
+        SET(MY_CMAKE_COMMAND ${CMAKE_INSTALL_DIR}/bin/cmake${CMAKE_EXECUTABLE_SUFFIX})
+    endif()
+    
+    # check if ssl library is installed on the system before building final cmake version
+    # Fixes the library detection, as no project has been initialized yet and thus
+    # the find_library wont work correctly. this will probably bite someone sometime :-|
+    if (UNIX)
+        SET(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES} .a .so)
+        SET(CMAKE_FIND_LIBRARY_PREFIXES ${CMAKE_FIND_LIBRARY_PREFIXES} lib)
+    endif()
+    find_package(OpenSSL QUIET)
+    # can't build cmake without OpenSSL (otherwise, no https downloads)
+    if (NOT OPENSSL_FOUND)
+        message(WARNING "No OpenSSL could be found on your system via normal check.\n"
+                        "Performing package config check now..\n")
+        find_package(PkgConfig REQUIRED)
+        pkg_search_module(OPENSSL QUIET openssl)
+
+        if (OPENSSL_FOUND)
+            message(STATUS "Found OpenSSL ${OPENSSL_VERSION}")
+            message(STATUS "OpenSSL include directory: ${OPENSSL_INCLUDE_DIR}")
+        else()
+            message(FATAL_ERROR "No OpenSSL could be found on your system via package config check.\n"
+                                "Building CMake required OpenSSL to be available.\n"
+                                "Please install OpenSSL before reinvoking the CMake build process.\n")
+        endif()
+    endif()
+    
+    # reset the paths for latest cmake version
+    SET(CMAKE_INSTALL_DIR ${OPENCMISS_ROOT}/install/utilities/cmake)
+    SET(CMAKE_TARBALL cmake-${CMAKE_MIN_VERSION}.tar.gz)
+    BUILD_CMAKE(${CMAKE_MIN_VERSION} ${CMAKE_MIN_VERSION_MAJ} YES)
+    SET(MY_CMAKE_COMMAND ${CMAKE_INSTALL_DIR}/bin/cmake${CMAKE_EXECUTABLE_SUFFIX})
+    
+    # remove intermediate cmake version if it was built
+    #file(REMOVE ${CMAKE_INTERMEDIATE_VERSION_INSTALL_DIR})
+    
+elseif(EXISTS ${MY_CMAKE_EXECUTABLE})
+    # TODO print message pointing to new binary.
+    message(STATUS "Using own build of CMake ${CMAKE_MIN_VERSION} at ${CMAKE_INSTALL_DIR}")
+    #SET(MY_CMAKE_COMMAND ${MY_CMAKE_EXECUTABLE})
+endif()
+        
+endif()
--- CMakeScripts/CMakeCheck.cmake
+++ CMakeScripts/CMakeCheck.cmake
@@ -1,175 +1,54 @@
 SET(CMAKE_MIN_VERSION_MAJ 3.2)
 SET(CMAKE_MIN_VERSION ${CMAKE_MIN_VERSION_MAJ}.0-rc1)
-message(STATUS "Checking CMake version..")
-# Use the cmake binary with which this script was invoked as default 
-SET(MY_CMAKE_COMMAND ${CMAKE_COMMAND})
 
-MACRO(BUILD_CMAKE VERSION_TO_BUILD VERSION_TO_BUILD_MAJ BUILD_WITH_OPENSSL)
-    # Download
-    if(NOT EXISTS ${CMAKE_SRC_DIR}/${CMAKE_TARBALL})
-        SET(CMAKE_SRC_TAR http://www.cmake.org/files/v${VERSION_TO_BUILD_MAJ}/${CMAKE_TARBALL})
-        message(STATUS "Downloading ${CMAKE_SRC_TAR}")
-        FILE(DOWNLOAD ${CMAKE_SRC_TAR} ${CMAKE_SRC_DIR}/${CMAKE_TARBALL})
-    endif()
-        
-    # Extract
-    message(STATUS "Extracting ${CMAKE_SRC_DIR}/${CMAKE_TARBALL} [${CMAKE_COMMAND} -E tar xzvf ${CMAKE_TARBALL}]")
-    execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${CMAKE_TARBALL} .
-        WORKING_DIRECTORY ${CMAKE_SRC_DIR})
-    file(REMOVE ${CMAKE_SRC_DIR}/${CMAKE_TARBALL})
-    
-    # Add top-level folder to src dir (default for cmake.org downloads)
-    SET(CMAKE_SRC_DIR "${CMAKE_SRC_DIR}/cmake-${VERSION_TO_BUILD}")
-    
-    # Build config
-    SET(CMAKE_DEFS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_DIR}
-        -DCMAKE_USE_OPENSSL=${BUILD_WITH_OPENSSL})
-    
-    # Create dir
-    SET(CMAKEBUILD_BINARY_DIR ${OPENCMISS_ROOT}/build/utilities/cmake-${VERSION_TO_BUILD})
-    file(MAKE_DIRECTORY ${CMAKEBUILD_BINARY_DIR})
-    
-    # Get build commands
-    include(OCMSetupBuildMacros)
-    GET_BUILD_COMMANDS(BUILD_COMMAND INSTALL_COMMAND ${CMAKEBUILD_BINARY_DIR} YES)
-    
-    # Run cmake
-    execute_process(COMMAND ${MY_CMAKE_COMMAND}
-        ${CMAKE_DEFS}
-        ${CMAKE_SRC_DIR}
-        WORKING_DIRECTORY ${CMAKEBUILD_BINARY_DIR}
-    )
-    
-    # Run build
-    #execute_process(COMMAND ${BUILD_COMMAND})
-    
-    # Run build/install
-    execute_process(COMMAND ${INSTALL_COMMAND})
-    
-    if (NOT EXISTS ${MY_CMAKE_EXECUTABLE})
-        message(FATAL_ERROR
-            "@@@@@@@@@ OpenCMISS utilities @@@@@@@@@\n"
-            "Building a newer CMake version failed. Please check & build manually.\n"
-            "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-        )
-    endif()
-ENDMACRO()
+message(STATUS "Checking CMake version..")
+# Assume we're good until found otherwise
+set(CMAKE_COMPATIBLE YES)
 
 # check if system cmake has ssl support
-include(HTTPSCheck)
+SET(CMAKE_HTTPS_TEST_URL "https://github.com/OpenCMISS-Utilities/gtest/blob/master/README.md")
+SET(CMAKE_HTTPS_TEST_DOWNLOAD_PATH "${OPENCMISS_ROOT}/build/cmake_https_test_download.txt")
+file(DOWNLOAD ${CMAKE_HTTPS_TEST_URL} ${CMAKE_HTTPS_TEST_DOWNLOAD_PATH} STATUS https_status TIMEOUT 60 INACTIVITY_TIMEOUT 60)
+list(GET https_status 0 HTTPS_TEST_DOWNLOAD_ERROR_CODE)
+list(GET https_status 1 HTTPS_TEST_DOWNLOAD_ERROR_STRING)
+# https download was successful
+if (HTTPS_TEST_DOWNLOAD_ERROR_CODE EQUAL 0)
+    SET(HTTPS_SUCCESS 1)
+else()
+    SET(HTTPS_SUCCESS 0)
+endif()
+# remove the test file as it will be created either way
+file(REMOVE ${CMAKE_HTTPS_TEST_DOWNLOAD_PATH})
 
 if (WIN32)
     if (CMAKE_VERSION VERSION_LESS ${CMAKE_MIN_VERSION})
-        message(FATAL_ERROR "Your CMake version is ${CMAKE_VERSION}.\n"
+        message(STATUS "Your CMake version is ${CMAKE_VERSION}.\n"
                             "At least version ${CMAKE_MIN_VERSION} is required for OpenCMISS.\n"
                             "Please download & install from http://www.cmake.org/download/\n")
+        set(CMAKE_COMPATIBLE NO)
     endif()
     if (NOT HTTPS_SUCCESS)
-        message(FATAL_ERROR "Your CMake version is ${CMAKE_VERSION} but does not support https downloads.\n"
-                            "At least version ${CMAKE_MIN_VERSION} is required for OpenCMISS.\n"
+        message(STATUS "Your CMake ${CMAKE_VERSION} up to date but does not support https downloads (SSL not builtin).\n"
                             "Please download & install from http://www.cmake.org/download/\n")
+        set(CMAKE_COMPATIBLE NO)                               
     endif()
 else()
-    # only build cmake if user specifically requires it
-    if (BUILD_CMAKE)
-        
-        SET(CMAKE_INSTALL_DIR ${OPENCMISS_ROOT}/install/utilities/cmake)
-        SET(MY_CMAKE_EXECUTABLE ${CMAKE_INSTALL_DIR}/bin/cmake${CMAKE_EXECUTABLE_SUFFIX})
-        
-        # Check if the binary is already there and hint the user to it
-        if (NOT EXISTS ${MY_CMAKE_EXECUTABLE})
-        
-            # Otherwise .. need compilation!
-            message(WARNING "Your CMake version is ${CMAKE_VERSION}, but at least version ${CMAKE_MIN_VERSION} is required for OpenCMISS. Building now..")
-            
-            SET(CMAKE_SRC_DIR ${OPENCMISS_ROOT}/src/utilities)
-            SET(CMAKE_INTERMEDIATE_VERSION_MAJ 2.8)
-            SET(CMAKE_INTERMEDIATE_VERSION ${CMAKE_INTERMEDIATE_VERSION_MAJ}.4)
-            
-            # compile intermediate version of cmake if present is too old
-            if (CMAKE_VERSION VERSION_LESS ${CMAKE_INTERMEDIATE_VERSION})
-                message(WARNING "Your CMake version is too old: ${CMAKE_VERSION}!\n"
-                                "A newer version is required to build version ${CMAKE_MIN_VERSION}. Building now..")
-                # set up the paths for an intermediate version of cmake
-                SET(CMAKE_TARBALL cmake-${CMAKE_INTERMEDIATE_VERSION}.tar.gz)
-                SET(CMAKE_INTERMEDIATE_VERSION_INSTALL_DIR ${OPENCMISS_ROOT}/install/utilities/cmake-${CMAKE_INTERMEDIATE_VERSION})
-                SET(CMAKE_INSTALL_DIR ${CMAKE_INTERMEDIATE_VERSION_INSTALL_DIR})
-                SET(MY_CMAKE_EXECUTABLE ${CMAKE_INSTALL_DIR}/bin/cmake${CMAKE_EXECUTABLE_SUFFIX})
-                # TODO: we could do a check again in case an intermediate version was built before?
-                BUILD_CMAKE(${CMAKE_INTERMEDIATE_VERSION} ${CMAKE_INTERMEDIATE_VERSION_MAJ} NO)
-                SET(MY_CMAKE_COMMAND ${CMAKE_INSTALL_DIR}/bin/cmake${CMAKE_EXECUTABLE_SUFFIX})
-            endif()
-            
-            # check if ssl library is installed on the system before building final cmake version
-            # Fixes the library detection, as no project has been initialized yet and thus
-            # the find_library wont work correctly. this will probably bite someone sometime :-|
-            if (UNIX)
-                SET(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES} .a .so)
-                SET(CMAKE_FIND_LIBRARY_PREFIXES ${CMAKE_FIND_LIBRARY_PREFIXES} lib)
-            endif()
-            find_package(OpenSSL QUIET)
-            # can't build cmake without OpenSSL (otherwise, no https downloads)
-            if (NOT OPENSSL_FOUND)
-                message(WARNING "No OpenSSL could be found on your system via normal check.\n"
-                                "Performing package config check now..\n")
-                find_package(PkgConfig REQUIRED)
-                pkg_search_module(OPENSSL QUIET openssl)
-
-                if (OPENSSL_FOUND)
-                    message(STATUS "Found OpenSSL ${OPENSSL_VERSION}")
-                    message(STATUS "OpenSSL include directory: ${OPENSSL_INCLUDE_DIR}")
-                else()
-                    message(FATAL_ERROR "No OpenSSL could be found on your system via package config check.\n"
-                                        "Building CMake required OpenSSL to be available.\n"
-                                        "Please install OpenSSL before reinvoking the CMake build process.\n")
-                endif()
-            endif()
-            
-            # reset the paths for latest cmake version
-            SET(CMAKE_INSTALL_DIR ${OPENCMISS_ROOT}/install/utilities/cmake)
-            SET(CMAKE_TARBALL cmake-${CMAKE_MIN_VERSION}.tar.gz)
-            BUILD_CMAKE(${CMAKE_MIN_VERSION} ${CMAKE_MIN_VERSION_MAJ} YES)
-            SET(MY_CMAKE_COMMAND ${CMAKE_INSTALL_DIR}/bin/cmake${CMAKE_EXECUTABLE_SUFFIX})
-            
-            # remove intermediate cmake version if it was built
-            #file(REMOVE ${CMAKE_INTERMEDIATE_VERSION_INSTALL_DIR})
-            
-        elseif(EXISTS ${MY_CMAKE_EXECUTABLE})
-            message(STATUS "Using own build of CMake ${CMAKE_MIN_VERSION} at ${CMAKE_INSTALL_DIR}")
-            #SET(MY_CMAKE_COMMAND ${MY_CMAKE_EXECUTABLE})
-        endif()
-        
-    # if user hasn't specified to build cmake,
-    # give hints, if version meets requirements and if ssl support is enabled
-    # otherwise, ask to install ssl libraries first
-    else()
-        # check for cmake version, ssl support and if OpenSSL is installed
-        if (CMAKE_VERSION VERSION_LESS ${CMAKE_MIN_VERSION})
-            if (NOT OPENSSL_FOUND)
-                message(FATAL_ERROR "Your CMake version is ${CMAKE_VERSION}.\n"
-                                    "At least version ${CMAKE_MIN_VERSION} is required for OpenCMISS.\n"
-                                    "No OpenSSL could be found on your system. Building CMake required OpenSSL to be available.\n"
-                                    "Please:\n"
-                                    " (1) Install OpenSSL.\n"
-                                    " (2) Reinvoke the CMake build process with -DBUILD_CMAKE=ON to build a newer version of CMake.\n")
-            else()
-                message(FATAL_ERROR "Your CMake version is ${CMAKE_VERSION}.\n"
-                                    "At least version ${CMAKE_MIN_VERSION} is required for OpenCMISS.\n"
-                                    "Please download & install a newer version of CMake or reinvoke the CMake build process with -DBUILD_CMAKE=ON to build a newer version of CMake.\n")
-            endif()
-        else()
-            if (NOT HTTPS_SUCCESS)
-                if (NOT OPENSSL_FOUND)
-                    message(FATAL_ERROR "Your CMake version is ${CMAKE_VERSION} but does not support OpenSSL.\n"
-                                        "No OpenSSL could be found on your system. Building CMake required OpenSSL to be available.\n"
-                                        "Please:\n"
-                                        " (1) Install OpenSSL.\n"
-                                        " (2) Reinvoke the CMake build process with -DBUILD_CMAKE=ON to build CMake with OpenSSL support.\n")
-                else()
-                    message(FATAL_ERROR "Your CMake version is ${CMAKE_VERSION} but does not support OpenSSL.\n"
-                                        "Please reinvoke the CMake build process with -DBUILD_CMAKE=ON to build CMake with OpenSSL support.\n")
-                endif()
-            endif()
-        endif()
+    # check for cmake version, ssl support and if OpenSSL is installed
+    if (CMAKE_VERSION VERSION_LESS ${CMAKE_MIN_VERSION})
+            message(STATUS "Your CMake version is ${CMAKE_VERSION}, but at least version ${CMAKE_MIN_VERSION} is required to build OpenCMISS.\n"
+                                "You can either:\n"
+                                " - Have your administrator install or update CMake ${CMAKE_MIN_VERSION} (or newer) with SSL support.\n"
+                                " - Build the target 'cmake'. This attempts to automatically download & compile a new CMake version for you.\n"
+                                "\n"
+                                "In any case, you need to re-invoke the CMake configuration step using the new CMake version.")
+            # Prepare "cmake" target that builds a new CMake version here
+            add_custom_target(cmake
+                COMMAND ${CMAKE_COMMAND}
+                    -DCMAKE_MIN_VERSION_MAJ=${CMAKE_MIN_VERSION_MAJ}
+                    -DCMAKE_MIN_VERSION=${CMAKE_MIN_VERSION}
+                    -DOPENCMISS_ROOT=${OPENCMISS_ROOT}
+                    -P ${OPENCMISS_MANAGE_DIR}/CMakeScripts/CMakeBuild.cmake  
+            )
+            set(CMAKE_COMPATIBLE NO)
     endif()
 endif()
