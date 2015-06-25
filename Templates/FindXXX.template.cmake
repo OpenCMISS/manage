622f0b4510a4af56081a3083bc5a611669fdfdda
Daniel Wirtz <daniel.wirtz@simtech.uni-stuttgart.de>
2015-06-25 18:02:33 +0200

    fixed find module wrappers: the late module path restoration could cause a subtle fatal error. now it's restored after any MODULE mode calls and stuff should work out

--- Templates/FindXXX.template.cmake
+++ Templates/FindXXX.template.cmake
@@ -34,16 +34,17 @@ else()
     message(STATUS "System search of component @PACKAGE_CASENAME@ enabled")
     
     # Remove all paths resolving to this one here so that recursive calls wont search here again
-    SET(_MODPATHCOPY ${CMAKE_MODULE_PATH})
-    SET(_READDME )
-    foreach(_ENTRY ${_MODPATHCOPY})
+    set(_ORIGINAL_CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH})
+    set(_READDME )
+    get_filename_component(_THIS_DIRECTORY ${CMAKE_CURRENT_LIST_FILE} DIRECTORY)
+    foreach(_ENTRY ${_ORIGINAL_CMAKE_MODULE_PATH})
         get_filename_component(_ENTRY_ABSOLUTE ${_ENTRY} ABSOLUTE)
-        get_filename_component(_PARENT_DIRECTORY ${CMAKE_CURRENT_LIST_FILE} DIRECTORY)
-        if (_ENTRY_ABSOLUTE STREQUAL _PARENT_DIRECTORY)
-            LIST(REMOVE_ITEM CMAKE_MODULE_PATH ${_ENTRY})
-            LIST(APPEND _READDME ${_ENTRY})
+        if (_ENTRY_ABSOLUTE STREQUAL _THIS_DIRECTORY)
+            list(REMOVE_ITEM CMAKE_MODULE_PATH ${_ENTRY})
         endif()
     endforeach()
+    unset(_THIS_DIRECTORY)
+    unset(_ENTRY_ABSOLUTE)
     
     # Make "native" call to find_package in MODULE mode first
     message(STATUS "Trying to find @PACKAGE_CASENAME@ ${@PACKAGE_CASENAME@_FIND_VERSION} in MODULE mode")
@@ -71,6 +72,19 @@ else()
     endif()
     unset(_readd)
     
+    # Restore the current module path
+    # This needs to be done BEFORE any calls in CONFIG find mode - if the found config has our
+    # xxx-config-dependencies, which in turn might be allowed as system lookup, the FindModuleWrapper dir
+    # is missing and stuff breaks. Too a while to figure out the problem as you might guess ;-)
+    # Scenario discovered on Michael Sprenger's Ubuntu 10 system with 
+    # OCM_SYSTEM_ZLIB=YES and found, OCM_SYSTEM_LIBXML2=ON but not found. This broke the CELLML-build as
+    # the wrapper call for LIBXML removed the wrapper dir from the module path, then found libxml2 in config mode,
+    # which in turn called find_dependency(ZLIB), which used the native FindZLIB instead of the wrapper first.
+    # This problem only was detected because the native zlib library is called "(lib)z", but we link against the 
+    # "zlib" target, which is either provided by our own build or by the wrapper that creates it. 
+    set(CMAKE_MODULE_PATH ${_ORIGINAL_CMAKE_MODULE_PATH})
+    unset(_ORIGINAL_CMAKE_MODULE_PATH)
+        
     if (@PACKAGE_NAME@_FOUND)
         # Also set the casename variant as this is checked upon at the end ("newer" version; config mode returns
         # a xXx_FOUND variable that has the same case as used for the call find_package(xXx ..)
@@ -123,7 +137,8 @@ else()
             message(STATUS "Find@PACKAGE_CASENAME@: Avoiding double import of target '@PACKAGE_TARGET@'")
         endif()
     else()
-        #message(STATUS "Trying to find @PACKAGE_CASENAME@ ${@PACKAGE_CASENAME@_FIND_VERSION} in CONFIG mode")
+        message(STATUS "Trying to find @PACKAGE_CASENAME@ ${@PACKAGE_CASENAME@_FIND_VERSION} in CONFIG mode")
+        
         # First look outside the prefix path
         #message(STATUS "find_package(@PACKAGE_CASENAME@ ${@PACKAGE_NAME@_FIND_VERSION} CONFIG QUIET NO_CMAKE_PATH)")
         find_package(@PACKAGE_CASENAME@ ${@PACKAGE_NAME@_FIND_VERSION} CONFIG QUIET NO_CMAKE_PATH)
@@ -143,12 +158,6 @@ else()
                 NO_CMAKE_SYSTEM_PACKAGE_REGISTRY)
         endif()
     endif()
-    
-    # Restore the current module path
-    # Sloppy as all are added to the front.. this will bite someone somewhere sometime :-p
-    foreach(_ENTRY ${_READDME})
-        LIST(INSERT CMAKE_MODULE_PATH 0 ${_ENTRY})
-    endforeach()
 endif()
 
 if (@PACKAGE_CASENAME@_FIND_REQUIRED AND NOT @PACKAGE_CASENAME@_FOUND)
