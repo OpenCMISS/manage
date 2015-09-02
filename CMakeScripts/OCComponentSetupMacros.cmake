0c135e24dfae1dfb18bcf56c1fb3b2932600ff5d
Hugh Sorby <h.sorby@auckland.ac.nz>
2015-09-03 02:04:15 +1200

    Replace spaces with tabs.

--- CMakeScripts/OCComponentSetupMacros.cmake
+++ CMakeScripts/OCComponentSetupMacros.cmake
@@ -71,7 +71,7 @@ function(addAndConfigureLocalComponent COMPONENT_NAME)
         endforeach()
     endif()
     
-	# Forward any other variables
+    # Forward any other variables
     foreach(extra_def ${ARGN})
         list(APPEND COMPONENT_DEFS -D${extra_def})
         #message(STATUS "${COMPONENT_NAME}: Using extra definition -D${extra_def}")
@@ -83,12 +83,12 @@ function(addAndConfigureLocalComponent COMPONENT_NAME)
     
     # Create the external projects
     createExternalProjects(${COMPONENT_NAME} "${COMPONENT_SOURCE}" "${COMPONENT_BUILD_DIR}" "${COMPONENT_DEFS}")
-	
-	# Create some convenience targets like clean, update etc
-	addConvenienceTargets(${COMPONENT_NAME} "${COMPONENT_BUILD_DIR}")
-		
-	# Add the dependency information for other downstream packages that might use this one
-	addDownstreamDependencies(${COMPONENT_NAME} TRUE)
+    
+    # Create some convenience targets like clean, update etc
+    addConvenienceTargets(${COMPONENT_NAME} "${COMPONENT_BUILD_DIR}")
+    
+    # Add the dependency information for other downstream packages that might use this one
+    addDownstreamDependencies(${COMPONENT_NAME} TRUE)
     
 endfunction()
 
@@ -187,9 +187,9 @@ function(createExternalProjects COMPONENT_NAME SOURCE_DIR BINARY_DIR DEFS)
     )
     
     # Add extra target that makes sure the source files are being present
-	# Triggers build of ${COMPONENT_NAME}_SRC if no CMakeLists.txt is found in the target source directory.
-	add_custom_target(CHECK_${COMPONENT_NAME}_SOURCES 
-	        COMMAND ${CMAKE_COMMAND}
+    # Triggers build of ${COMPONENT_NAME}_SRC if no CMakeLists.txt is found in the target source directory.
+    add_custom_target(CHECK_${COMPONENT_NAME}_SOURCES 
+        COMMAND ${CMAKE_COMMAND}
                 -DCOMPONENT=${COMPONENT_NAME}
                 -DFOLDER=${SOURCE_DIR}
                 -DBINDIR=${CMAKE_CURRENT_BINARY_DIR}
@@ -199,77 +199,77 @@ function(createExternalProjects COMPONENT_NAME SOURCE_DIR BINARY_DIR DEFS)
     )  
     
     #message(STATUS "Adding ${COMPONENT_NAME} with DEPS=${${COMPONENT_NAME}_DEPS}, DEFS=${DEFS}")    
-	ExternalProject_Add(${COMPONENT_NAME}
-		DEPENDS ${${COMPONENT_NAME}_DEPS} CHECK_${COMPONENT_NAME}_SOURCES
-		PREFIX ${BINARY_DIR}
-		LIST_SEPARATOR ${OCM_LIST_SEPARATOR}
-		TMP_DIR ${BINARY_DIR}/ep_tmp
-		STAMP_DIR ${BINARY_DIR}/ep_stamps
-		
-		#--Download step--------------
-		# Ideal solution - include in the external project that also builds.
-		# Still a mess with mixed download/build stamp files and even though the UPDATE_DISCONNECTED command
-		# skips the update command the configure etc dependency chain is yet executed each time :-(
+    ExternalProject_Add(${COMPONENT_NAME}
+        DEPENDS ${${COMPONENT_NAME}_DEPS} CHECK_${COMPONENT_NAME}_SOURCES
+        PREFIX ${BINARY_DIR}
+        LIST_SEPARATOR ${OCM_LIST_SEPARATOR}
+        TMP_DIR ${BINARY_DIR}/ep_tmp
+        STAMP_DIR ${BINARY_DIR}/ep_stamps
+        
+        #--Download step--------------
+        # Ideal solution - include in the external project that also builds.
+        # Still a mess with mixed download/build stamp files and even though the UPDATE_DISCONNECTED command
+        # skips the update command the configure etc dependency chain is yet executed each time :-(
         #${DOWNLOAD_CMDS}
         #UPDATE_DISCONNECTED 1 # Dont update without being asked. New feature of CMake 3.2.0-rc1
-		
+        
         # Need empty download command, otherwise creation of external project fails with "no download info"
-		DOWNLOAD_COMMAND ""
-		
-		#--Configure step-------------
-		CMAKE_COMMAND ${CMAKE_COMMAND} --no-warn-unused-cli # disables warnings for unused cmdline options
-		SOURCE_DIR ${SOURCE_DIR}
-		BINARY_DIR ${BINARY_DIR}
-		CMAKE_ARGS ${DEFS}
-		
-		#--Build step-----------------
-		BUILD_COMMAND ${BUILD_COMMAND}
-		#--Install step---------------
-		# currently set as extra arg (above), somehow does not work
-		#INSTALL_DIR ${CMAKE_INSTALL_PREFIX} 
-		INSTALL_COMMAND ${INSTALL_COMMAND}
-		# Logging
-		LOG_CONFIGURE ${_LOGFLAG}
-		LOG_BUILD ${_LOGFLAG}
-		LOG_INSTALL ${_LOGFLAG}
-	)
-		
-	# See OpenCMISSDeveloper.cmake
-	if (OCM_CLEAN_REBUILDS_COMPONENTS)
+        DOWNLOAD_COMMAND ""
+        
+        #--Configure step-------------
+        CMAKE_COMMAND ${CMAKE_COMMAND} --no-warn-unused-cli # disables warnings for unused cmdline options
+        SOURCE_DIR ${SOURCE_DIR}
+        BINARY_DIR ${BINARY_DIR}
+        CMAKE_ARGS ${DEFS}
+        
+        #--Build step-----------------
+        BUILD_COMMAND ${BUILD_COMMAND}
+        #--Install step---------------
+        # currently set as extra arg (above), somehow does not work
+        #INSTALL_DIR ${CMAKE_INSTALL_PREFIX} 
+        INSTALL_COMMAND ${INSTALL_COMMAND}
+        # Logging
+        LOG_CONFIGURE ${_LOGFLAG}
+        LOG_BUILD ${_LOGFLAG}
+        LOG_INSTALL ${_LOGFLAG}
+    )
+        
+    # See OpenCMISSDeveloper.cmake
+    if (OCM_CLEAN_REBUILDS_COMPONENTS)
         set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES ${BINARY_DIR}/CMakeCache.txt)
     endif()
 endfunction()
 
 function(addConvenienceTargets COMPONENT_NAME BINARY_DIR)
     # Add convenience direct-access clean target for component
-	add_custom_target(${COMPONENT_NAME}-clean
-	    COMMAND ${CMAKE_COMMAND} -E remove -f ${BINARY_DIR}/ep_stamps/*-configure 
-	    COMMAND ${CMAKE_COMMAND} --build ${BINARY_DIR} --target clean
-	    COMMAND ${CMAKE_COMMAND} -E remove -f ${BINARY_DIR}/CMakeCache.txt
-	    COMMENT "Cleaning ${COMPONENT_NAME}"
-	)
-	# Add convenience direct-access update target for component
-	# This removes the external project stamp for the last update and hence triggers execution of the update, e.g.
-	# a new source download or "git pull"
-	add_custom_target(${COMPONENT_NAME}-update
-	    COMMAND ${CMAKE_COMMAND} -E remove -f ${OPENCMISS_ROOT}/src/download/stamps/${COMPONENT_NAME}_SRC-update
-	    COMMAND ${CMAKE_COMMAND} --build ${CMAKE_CURRENT_BINARY_DIR} --target ${COMPONENT_NAME}_SRC-update
-	    COMMENT "Updating ${COMPONENT_NAME} sources"
-	)
-	# Add convenience direct-access forced build target for component
-	getBuildCommands(_DUMMY INSTALL_COMMAND ${BINARY_DIR} TRUE)
-	add_custom_target(${COMPONENT_NAME}-build
-	    COMMAND ${CMAKE_COMMAND} -E remove -f ${BINARY_DIR}/ep_stamps/*-build 
-	    COMMAND ${INSTALL_COMMAND}
-	)
-	if (BUILD_TESTS)
-    	# Add convenience direct-access test target for component
-    	add_custom_target(${COMPONENT_NAME}-test
-    	    COMMAND ${CMAKE_COMMAND} --build ${BINARY_DIR} --target test
-    	)
-    	# Add a global test to run the external project's tests
-    	add_test(${COMPONENT_NAME}-test ${CMAKE_COMMAND} --build ${BINARY_DIR} --target test)
-	endif()
+    add_custom_target(${COMPONENT_NAME}-clean
+        COMMAND ${CMAKE_COMMAND} -E remove -f ${BINARY_DIR}/ep_stamps/*-configure 
+        COMMAND ${CMAKE_COMMAND} --build ${BINARY_DIR} --target clean
+        COMMAND ${CMAKE_COMMAND} -E remove -f ${BINARY_DIR}/CMakeCache.txt
+        COMMENT "Cleaning ${COMPONENT_NAME}"
+    )
+    # Add convenience direct-access update target for component
+    # This removes the external project stamp for the last update and hence triggers execution of the update, e.g.
+    # a new source download or "git pull"
+    add_custom_target(${COMPONENT_NAME}-update
+        COMMAND ${CMAKE_COMMAND} -E remove -f ${OPENCMISS_ROOT}/src/download/stamps/${COMPONENT_NAME}_SRC-update
+        COMMAND ${CMAKE_COMMAND} --build ${CMAKE_CURRENT_BINARY_DIR} --target ${COMPONENT_NAME}_SRC-update
+        COMMENT "Updating ${COMPONENT_NAME} sources"
+    )
+    # Add convenience direct-access forced build target for component
+    getBuildCommands(_DUMMY INSTALL_COMMAND ${BINARY_DIR} TRUE)
+    add_custom_target(${COMPONENT_NAME}-build
+        COMMAND ${CMAKE_COMMAND} -E remove -f ${BINARY_DIR}/ep_stamps/*-build 
+        COMMAND ${INSTALL_COMMAND}
+    )
+    if (BUILD_TESTS)
+    # Add convenience direct-access test target for component
+    add_custom_target(${COMPONENT_NAME}-test
+        COMMAND ${CMAKE_COMMAND} --build ${BINARY_DIR} --target test
+    )
+    # Add a global test to run the external project's tests
+    add_test(${COMPONENT_NAME}-test ${CMAKE_COMMAND} --build ${BINARY_DIR} --target test)
+    endif()
 endfunction()
 
 function(getBuildCommands BUILD_CMD_VAR INSTALL_CMD_VAR DIR PARALLEL)
@@ -305,8 +305,8 @@ function(getBuildCommands BUILD_CMD_VAR INSTALL_CMD_VAR DIR PARALLEL)
         endif()
     endif()
 
-	SET(${BUILD_CMD_VAR} ${BUILD_CMD} PARENT_SCOPE)
-	SET(${INSTALL_CMD_VAR} ${INSTALL_CMD} PARENT_SCOPE)
+    SET(${BUILD_CMD_VAR} ${BUILD_CMD} PARENT_SCOPE)
+    SET(${INSTALL_CMD_VAR} ${INSTALL_CMD} PARENT_SCOPE)
 endfunction()
 
 # IN_PARENT_SCOPE: Set results in PARENT_SCOPE.
