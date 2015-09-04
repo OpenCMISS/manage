07cdbe024ce795a785ff93fbb9e81530509520c6
Daniel Wirtz <daniel.wirtz@simtech.uni-stuttgart.de>
2015-09-04 16:37:14 +0200

    passing version numbers of consumed components added freetype to own builds increased robustness of XXX_FOUND variables (always available upper case now)

--- CMakeScripts/OCInstallFindModuleWrappers.cmake
+++ CMakeScripts/OCInstallFindModuleWrappers.cmake
@@ -2,11 +2,12 @@
 SET(PACKAGES_WITH_TARGETS BLAS HYPRE LAPACK METIS
     MUMPS PARMETIS PASTIX PETSC PLAPACK PTSCOTCH SCALAPACK
     SCOTCH SOWING SUITESPARSE SUNDIALS SUPERLU SUPERLU_DIST ZLIB SZIP HDF5
-    BZIP2 LIBXML2 JPEG OPTPP NETGEN GLEW PNG FTGL)
+    BZIP2 LIBXML2 JPEG OPTPP NETGEN GLEW PNG FTGL FREETYPE)
     
 # Some shipped find-package modules have a different case-sensitive spelling - need to stay consistent with that
 SET(LIBXML2_CASENAME LibXml2)
 SET(BZIP2_CASENAME BZip2)
+SET(FREETYPE_CASENAME Freetype)
 # Some packages naturally have their exported target names differ from those of the package - this is convenience but
 # enables us to stay more consistent (e.g. we have "libbz2.a" on system installations instead of "libbzip2.a")
 SET(LIBXML2_TARGETNAME xml2)
