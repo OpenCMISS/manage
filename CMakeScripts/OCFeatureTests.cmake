a460b5c67a13663005f67a619f963b179a47207c
Daniel Wirtz <daniel.wirtz@simtech.uni-stuttgart.de>
2015-09-09 13:51:31 +0200

    improved feature test invocation

--- CMakeScripts/OCFeatureTests.cmake
+++ CMakeScripts/OCFeatureTests.cmake
@@ -1,8 +1,14 @@
 set(FEATURE_TEST_EXAMPLES 
     classicalfield_laplace_laplace_fortran
     finiteelasticity_cantilever_fortran
-    bioelectrics_monodomain_fortran
 )
+if (OCM_USE_CELLML AND IRON_WITH_CELLML)
+    list(APPEND FEATURE_TEST_EXAMPLES bioelectrics_monodomain_fortran)
+endif()
+# Collect any arguments
+# n98.xml is the only currently working xml file
+set(bioelectrics_monodomain_fortran_ARGS 0.005 0.1001 70 n98.xml)
+
 set(BIN_DIR_ROOT ${OPENCMISS_COMPONENTS_BINARY_DIR_MPI}/featuretests)
 set(SRC_DIR_ROOT ${OPENCMISS_ROOT}/src/featuretests)
 set(GITHUB_ORGANIZATION OpenCMISS-Examples)
@@ -17,10 +23,12 @@ foreach(example_name ${FEATURE_TEST_EXAMPLES})
     createExternalProjects(${example_name} "${SRC_DIR}" "${BIN_DIR}" "${DEFS}")
     
     add_test(NAME feature_${example_name}
-        COMMAND run
+        COMMAND run ${${example_name}_ARGS}
         WORKING_DIRECTORY ${SRC_DIR}     
     )
 endforeach()
+# Add a top level target that runs only the feature tests
 add_custom_target(featuretests
     COMMAND ${CMAKE_CTEST_COMMAND} feature_*
+    COMMENT "Running OpenCMISS feature tests"
 )
