83977a147f8865ee5b07f544c20fe78f2da123c7
Daniel Wirtz <daniel.wirtz@simtech.uni-stuttgart.de>
2016-01-25 13:59:54 +1300

    added inclusion of global site-packages into the virtual environment this should help with numpy installations (no local copies)

--- CMakeScripts/OCPythonBindings.cmake
+++ CMakeScripts/OCPythonBindings.cmake
@@ -6,7 +6,7 @@ if (IRON_WITH_Python_BINDINGS OR ZINC_WITH_Python_BINDINGS)
         if (VIRTUALENV_EXECUTABLE)
             set(PYTHON_VIRTUALENV_DIR ${OPENCMISS_COMPONENTS_INSTALL_PREFIX_MPI}/python)
             add_custom_target(python_virtualenv ALL
-                COMMAND ${VIRTUALENV_EXECUTABLE} "${PYTHON_VIRTUALENV_DIR}"
+                COMMAND ${VIRTUALENV_EXECUTABLE} --system-site-packages "${PYTHON_VIRTUALENV_DIR}"
             )
         else()
             log("Python bindings will be generated but virtualenv is not available. Convenience binding switching will not be available.\nSee http://www.opencmiss.org/documentation for more information" WARNING)
