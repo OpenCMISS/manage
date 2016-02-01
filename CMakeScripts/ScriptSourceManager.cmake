75edb00c23e7fcd4266bb5ba003637b87e2fa21d
Daniel Wirtz <daniel.wirtz@simtech.uni-stuttgart.de>
2016-02-02 11:13:54 +1300

    also making sure thee source directory is created upon running source presence checks

--- CMakeScripts/ScriptSourceManager.cmake
+++ CMakeScripts/ScriptSourceManager.cmake
@@ -8,6 +8,7 @@ if (MODE STREQUAL "Check")
     # Passed variables are: SRC_DIR, BIN_DIR and COMPONENT
     if (NOT EXISTS "${SRC_DIR}/CMakeLists.txt")
         execute_process(
+	    COMMAND ${CMAKE_COMMAND} -E make_directory "${SRC_DIR}"
             COMMAND ${CMAKE_COMMAND} --build "${BIN_DIR}" --target ${COMPONENT}-download
             RESULT_VARIABLE RES
             ERROR_VARIABLE ERR
