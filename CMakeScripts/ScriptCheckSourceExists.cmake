c2a9f9dff831a56ed212584c2b39227c578e154c
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-09-05 15:10:41 +1200

    Remove CMake modules from this repository.

--- CMakeScripts/ScriptCheckSourceExists.cmake
+++
@@ -1,8 +0,0 @@
-# Script called within the component configuration to ensure if the associated source files are downloaded
-if (NOT EXISTS "${FOLDER}/CMakeLists.txt")
-    execute_process(
-        COMMAND ${CMAKE_COMMAND} -E remove "${STAMP_DIR}/${COMPONENT}_SRC-download"
-        COMMAND ${CMAKE_COMMAND} -E remove "${STAMP_DIR}/${COMPONENT}_SRC-gitclone-lastrun.txt"
-        COMMAND ${CMAKE_COMMAND} --build ${BINDIR} --target ${COMPONENT}_SRC-download
-    )
-endif()
