07542cea1c3864bc02f2f529fe7ebf1dde4287e5
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-12-22 12:56:31 +1300

    Remove spaces in empty lines.

--- LibrariesConfig/Modules/OCComponentSetupMacros.cmake
+++ LibrariesConfig/Modules/OCComponentSetupMacros.cmake
@@ -6,7 +6,7 @@
 function(addAndConfigureLocalComponent COMPONENT_NAME)
     # Get lowercase folder name from project name
     string(TOLOWER ${COMPONENT_NAME} FOLDER_NAME)
-    
+
     # Keep track of self-build components (thus far only used for "update" target)
     #list(APPEND _OC_SELECTED_COMPONENTS ${COMPONENT_NAME})
     # Need this since it's a function
@@ -38,31 +38,31 @@ function(addAndConfigureLocalComponent COMPONENT_NAME)
     set(COMPONENT_BUILD_DIR ${BUILD_DIR_BASE}/${FOLDER_NAME}/${BUILD_TYPE_PATH_ELEM})
     # Expose the current build directory outside the function - used only for Iron and Zinc yet 
     set(${COMPONENT_NAME}_BINARY_DIR ${COMPONENT_BUILD_DIR} PARENT_SCOPE)
-    
+
     ##############################################################
     # Verifications
     if(COMPONENT_NAME IN_LIST OPENCMISS_COMPONENTS_WITH_F90 AND NOT CMAKE_Fortran_COMPILER_SUPPORTS_F90)
         log("Your Fortran compiler ${CMAKE_Fortran_COMPILER} does not support the Fortran 90 standard,
             which is required to build the OpenCMISS component ${COMPONENT_NAME}" ERROR)
     endif()
-    
+
     ##############################################################
     # Collect component definitions
-    SET(COMPONENT_DEFS
+    set(COMPONENT_DEFS
         ${COMPONENT_COMMON_DEFS} 
         -DCMAKE_INSTALL_PREFIX:STRING=${_INSTALL_PREFIX}
-        )
-    
+    )
+
     # Shared or static?
     list(APPEND COMPONENT_DEFS -DBUILD_SHARED_LIBS=${${COMPONENT_NAME}_SHARED})
-    
+
     # OpenMP multithreading
     if(COMPONENT_NAME IN_LIST OPENCMISS_COMPONENTS_WITH_OPENMP)
         list(APPEND COMPONENT_DEFS
             -DWITH_OPENMP=${OC_MULTITHREADING}
         )
     endif()
-    
+
     # check if MPI compilers should be forwarded/set
     # so that the local FindMPI uses that
     if(${COMPONENT_NAME} IN_LIST OPENCMISS_COMPONENTS_WITHMPI)
@@ -101,32 +101,32 @@ function(addAndConfigureLocalComponent COMPONENT_NAME)
             )
         endforeach()
     endif()
-    
+
     # Forward any other variables
     foreach(extra_def ${ARGN})
         list(APPEND COMPONENT_DEFS -D${extra_def})
     endforeach()
     log("OpenCMISS component ${COMPONENT_NAME} extra args:\n${COMPONENT_DEFS}" DEBUG)
-    
+
     ##############################################################
     # Create actual external projects
-    
+
     # Create the external projects
     createExternalProjects(${COMPONENT_NAME} "${COMPONENT_SOURCE}" "${COMPONENT_BUILD_DIR}" "${COMPONENT_DEFS}")
-    
+
     # Create some convenience targets like clean, update etc
     addConvenienceTargets(${COMPONENT_NAME} "${COMPONENT_BUILD_DIR}" "${COMPONENT_SOURCE}")
-    
+
     # Add the dependency information for other downstream packages that might use this one
     addDownstreamDependencies(${COMPONENT_NAME} TRUE)
-    
+
 endfunction()
 
 ########################################################################################################################
 function(addSourceManagementTargets COMPONENT_NAME BINARY_DIR SOURCE_DIR)
     # Convention: github repo is the lowercase equivalent of the component name
     string(TOLOWER ${COMPONENT_NAME} REPO_NAME)
-    
+
     # Git clone mode
     if(GIT_FOUND)
         # Construct the repository name 
@@ -143,14 +143,14 @@ function(addSourceManagementTargets COMPONENT_NAME BINARY_DIR SOURCE_DIR)
             endif()
             set(${COMPONENT_NAME}_REPO ${GITHUB_PROTOCOL}${_GITHUB_USERNAME}/${REPO_NAME})
         endif()
-        
+
         add_custom_target(${OC_SM_PREFIX}${REPO_NAME}_download
             COMMAND ${GIT_EXECUTABLE} clone ${${COMPONENT_NAME}_REPO} .
             COMMAND ${GIT_EXECUTABLE} checkout ${${COMPONENT_NAME}_BRANCH}
             COMMENT "Cloning ${COMPONENT_NAME} sources"
             WORKING_DIRECTORY "${SOURCE_DIR}"
         )
-        
+
         add_custom_target(${OC_SM_PREFIX}${REPO_NAME}_update
             DEPENDS ${OC_SM_PREFIX}${COMPONENT_NAME}_sources
             COMMAND ${GIT_EXECUTABLE} pull
@@ -159,14 +159,14 @@ function(addSourceManagementTargets COMPONENT_NAME BINARY_DIR SOURCE_DIR)
             COMMENT "Updating ${COMPONENT_NAME} sources"
             WORKING_DIRECTORY "${SOURCE_DIR}"
         )
-            
+
     # Fallback: Download the current version branch as zip
     else()
         # Unless explicitly specified, use the GitHub repository location
         if (NOT ${COMPONENT_NAME}_REPO)
             set(${COMPONENT_NAME}_REPO https://github.com/${GITHUB_ORGANIZATION}/${REPO_NAME})
         endif()
-        
+
         set(_FILENAME ${${COMPONENT_NAME}_BRANCH}.tar.gz)
         add_custom_target(${OC_SM_PREFIX}${REPO_NAME}_download
             COMMAND ${CMAKE_COMMAND}
@@ -176,7 +176,7 @@ function(addSourceManagementTargets COMPONENT_NAME BINARY_DIR SOURCE_DIR)
                 -P ${CMAKE_CURRENT_SOURCE_DIR}/Scripts/OCSourceManager.cmake
             COMMENT "Downloading ${COMPONENT_NAME} sources"
         )
-        
+
         # For tarballs, update is the same as download!
         add_custom_target(${OC_SM_PREFIX}${REPO_NAME}_update
             DEPENDS ${OC_SM_PREFIX}${REPO_NAME}_download
@@ -186,7 +186,7 @@ function(addSourceManagementTargets COMPONENT_NAME BINARY_DIR SOURCE_DIR)
     endif()
     set_target_properties(${OC_SM_PREFIX}${REPO_NAME}_download PROPERTIES FOLDER "Source management")
     set_target_properties(${OC_SM_PREFIX}${REPO_NAME}_update PROPERTIES FOLDER "Source management")
-    
+
     # Add extra target that makes sure the source files are being present
     # Triggers build of ${OC_SM_PREFIX}${REPO_NAME}_download if the directory does not exist or 
     # no CMakeLists.txt is found in the target source directory.
@@ -201,7 +201,7 @@ function(addSourceManagementTargets COMPONENT_NAME BINARY_DIR SOURCE_DIR)
         COMMENT "Checking ${COMPONENT_NAME} sources are present"
     )
     set_target_properties(${OC_SM_PREFIX}${COMPONENT_NAME}_sources PROPERTIES FOLDER "Internal")
-    
+
     add_custom_target(${OC_SM_PREFIX}${REPO_NAME}_update_force
         COMMAND ${CMAKE_COMMAND} -E remove_directory "${SOURCE_DIR}"
         COMMAND ${CMAKE_COMMAND} -E make_directory "${SOURCE_DIR}"
@@ -209,7 +209,7 @@ function(addSourceManagementTargets COMPONENT_NAME BINARY_DIR SOURCE_DIR)
         COMMENT "Forced update of ${COMPONENT_NAME} - removing and downloading"
     )
     set_target_properties(${OC_SM_PREFIX}${REPO_NAME}_update_force PROPERTIES FOLDER "Source management")
-    
+
 endfunction()
 
 ########################################################################################################################
@@ -218,7 +218,7 @@ function(createExternalProjects COMPONENT_NAME SOURCE_DIR BINARY_DIR DEFS)
     log("Configuring build of '${COMPONENT_NAME} ${${COMPONENT_NAME}_VERSION}' in ${BINARY_DIR}...")
 
     getBuildCommands(BUILD_COMMAND INSTALL_COMMAND ${BINARY_DIR} TRUE)
-    
+
     if (COMPONENT_NAME STREQUAL "EXAMPLES")
         # Special hack for the examples - we want the build to continue even if some examples wont compile.
         # TODO remove later.    
@@ -229,16 +229,16 @@ function(createExternalProjects COMPONENT_NAME SOURCE_DIR BINARY_DIR DEFS)
     #else()
     #    set(INSTALL_COMMAND INSTALL_COMMAND ${INSTALL_COMMAND})
     endif()
-        
+
     addSourceManagementTargets(${COMPONENT_NAME} ${BINARY_DIR} ${SOURCE_DIR})
-    
+
     # Log settings
     if (OC_CREATE_LOGS)
         set(_LOGFLAG 1)
     else()
         set(_LOGFLAG 0)
     endif()  
-    
+
     log("Adding ${COMPONENT_NAME} with DEPS=${${COMPONENT_NAME}_DEPS}" VERBOSE)
     ExternalProject_Add(${OC_EP_PREFIX}${COMPONENT_NAME}
         DEPENDS ${${COMPONENT_NAME}_DEPS} ${OC_SM_PREFIX}${COMPONENT_NAME}_sources
@@ -246,24 +246,24 @@ function(createExternalProjects COMPONENT_NAME SOURCE_DIR BINARY_DIR DEFS)
         LIST_SEPARATOR ${OC_LIST_SEPARATOR}
         TMP_DIR ${BINARY_DIR}/${OC_EXTPROJ_TMP_DIR}
         STAMP_DIR ${BINARY_DIR}/${OC_EXTPROJ_STAMP_DIR}
-        
+
         #--Download step--------------
         # Ideal solution - include in the external project that also builds.
         # Still a mess with mixed download/build stamp files and even though the UPDATE_DISCONNECTED command
         # skips the update command the configure etc dependency chain is yet executed each time :-(
         #${DOWNLOAD_CMDS}
         #UPDATE_DISCONNECTED 1 # Dont update without being asked. New feature of CMake 3.2.0-rc1
-        
+
         # Need empty download command, otherwise creation of external project fails with "no download info"
         DOWNLOAD_COMMAND ""
-        
+
         #--Configure step-------------
         # CONFIGURE_COMMAND "" # Do nothing remove soon
         CMAKE_COMMAND ${CMAKE_COMMAND} --no-warn-unused-cli # disables warnings for unused cmdline options
         SOURCE_DIR ${SOURCE_DIR}
         BINARY_DIR ${BINARY_DIR}
         CMAKE_ARGS ${DEFS}
-        
+
         #--Build step-----------------
         BUILD_COMMAND ${BUILD_COMMAND}
         #--Install step---------------
@@ -276,27 +276,27 @@ function(createExternalProjects COMPONENT_NAME SOURCE_DIR BINARY_DIR DEFS)
         LOG_INSTALL ${_LOGFLAG}
     )
     set_target_properties(${OC_EP_PREFIX}${COMPONENT_NAME} PROPERTIES FOLDER "ExternalProjects")
-        
+
     # See OpenCMISSInstallationConfig.cmake
     if (OC_CLEAN_REBUILDS_COMPONENTS)
         set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES ${BINARY_DIR}/CMakeCache.txt)
     endif()
-    
+
     doSupportStuff(${COMPONENT_NAME} "${SOURCE_DIR}" "${BINARY_DIR}" "${DEFS}")
-    
+
 endfunction()
 
 include(OCFunctionComponentTargets)
 
 function(getBuildCommands BUILD_CMD_VAR INSTALL_CMD_VAR DIR PARALLEL)
-    
+
     set(BUILD_CMD ${CMAKE_COMMAND} --build "${DIR}")
     set(INSTALL_CMD ${CMAKE_COMMAND} --build "${DIR}" --target install)
     if (OPENCMISS_HAVE_MULTICONFIG_ENV)
         list(APPEND BUILD_CMD --config $<CONFIG>)
         list(APPEND INSTALL_CMD --config $<CONFIG>)
     endif()
-    
+
     if(PARALLEL_BUILDS AND PARALLEL)
         include(ProcessorCount)
         ProcessorCount(NUM_PROCESSORS)
@@ -305,7 +305,7 @@ function(getBuildCommands BUILD_CMD_VAR INSTALL_CMD_VAR DIR PARALLEL)
         #else()
         #    MATH(EXPR NUM_PROCESSORS ${NUM_PROCESSORS}+4)
         endif()
-        
+
         if (CMAKE_GENERATOR MATCHES "^Visual Studio")
             SET(GENERATOR_MATCH_VISUAL_STUDIO TRUE)
         elseif(CMAKE_GENERATOR MATCHES "^NMake Makefiles$")
@@ -315,7 +315,7 @@ function(getBuildCommands BUILD_CMD_VAR INSTALL_CMD_VAR DIR PARALLEL)
             OR CMAKE_GENERATOR MATCHES "^MSYS Makefiles$")
             SET(GENERATOR_MATCH_MAKE TRUE)
         endif()
-        
+
         if(GENERATOR_MATCH_MAKE OR GENERATOR_MATCH_NMAKE)
             LIST(APPEND BUILD_CMD -- "-j${NUM_PROCESSORS}")
             LIST(APPEND INSTALL_CMD -- "-j${NUM_PROCESSORS}")
@@ -355,7 +355,7 @@ function(doSupportStuff NAME SRC BIN DEFS)
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 Build configuration file for OpenCMISS component ${NAME}-${${NAME}_VERSION}
-    
+
 Source directory '${SRC}'
 Build directory '${BIN}'
 
@@ -364,7 +364,7 @@ Configure definitions:
     foreach(_DEF ${DEFS})
         file(APPEND "${SUPPORT_FILE}" "${_DEF}\r\n")
     endforeach()
-    
+
     # Only create log-collecting commands if we create them 
     if (OC_CREATE_LOGS)
         # Using PRE_BUILD directly for target support does not work :-| See docs.
@@ -379,7 +379,7 @@ Configure definitions:
         add_dependencies(collect_logs ${OC_SM_PREFIX}${NAME}_collect_log)
         set_target_properties(${OC_SM_PREFIX}${NAME}_collect_log PROPERTIES FOLDER "Internal")
     endif()
-    
+
     add_custom_target(${OC_SM_PREFIX}${NAME}_build_log
         COMMAND ${CMAKE_COMMAND}
             -DBUILD_STAMP=YES 
