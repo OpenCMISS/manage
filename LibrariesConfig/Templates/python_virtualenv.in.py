11384666a8bb5c3974532b0c5617d0787aac5166
Hugh Sorby <h.sorby@auckland.ac.nz>
2017-04-11 12:25:07 +1200

    Remove redundant files 'python_virtualenv.in.py' and 'opencmiss.bindings.in.py'.

--- LibrariesConfig/Templates/python_virtualenv.in.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# This file contains information about installed virtual environments 
-
-info = { "dir": "@_OC_PYTHON_INSTALL_PREFIX@", "toolchain": "@_ACTIVE_TOOLCHAIN@",
-         "mpi": "@_ACTIVE_MPI@", "mpi_home": "@_ACTIVE_MPI_HOME@", "buildtype": "@BTYPE@",
-         "compiler": "@COMPILER@", "mpi_buildtype": "@_ACTIVE_MPI_BUILD_TYPE@",
-         "library_path": "@LIBRARY_PATH@", "is_virtual_env": @IS_VIRTUALENV@, "activate": "@ACTIVATE_SCRIPT@", }
-
